001f0b0370e2dec335a2f8ec6c6727b885171135
Felipe Tomm <felipe.tomm@syntesis.com.br>
2020-11-27 15:38:54 -0300

    Changed version for publish

--- dist/lib/log.js
+++ dist/lib/log.js
@@ -1,26 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function getDateTime() {
-    const date = new Date();
-    const response = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    return response;
-}
 /** Temp logging, should override form adapter's log */
 class InternalLog {
     debug(...args) {
-        console.log([`[debug] ${getDateTime()}: `, ...args]);
+        console.log(...args);
     }
     info(...args) {
-        console.log([`[info] ${getDateTime()}: `, ...args]);
+        console.log(...args);
     }
     warning(...args) {
-        console.warn([`[warning] ${getDateTime()}: `, ...args]);
+        console.warn(...args);
     }
     warn(...args) {
         return this.warning(...args);
     }
     error(...args) {
-        console.error([`[error] ${getDateTime()}: `, ...args]);
+        console.error(...args);
     }
 }
 let logger = new InternalLog();
